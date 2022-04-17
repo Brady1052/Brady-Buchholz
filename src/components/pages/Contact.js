a9961efb6646cb883254bfacaf927d052719f017
Brady Buchholz <bradybuchholz@gmail.com>
2022-04-16 21:52:55 -0400

    updated Readme, and used prettier on the contact.js file

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import GitHub from '../../images/GitHub.png'
-import LinkedIn from '../../images/LinkedIn_Logo.png'
+import GitHub from '../../images/GitHub.png';
+import LinkedIn from '../../images/LinkedIn_Logo.png';
 export default function Contact() {
   return (
     <div>
-        <h1>Phone Number:</h1>
-        <p id="phoneNumber">(614) 586-3474</p>
-        <h1>Email Address:</h1>
-        <a href='mailto:bradybuchholz.dev@gmail.com' id="emailAddress">BradyBuchholz.dev@gmail.com</a><br></br>
-<br></br>
-<h1>GitHub</h1>
-      <a href='https://github.com/Brady1052' target='_blank' rel='noreferrer'><img src={GitHub} id='gitHub'  alt='GitHub Profile'></img></a>
-<div>
-  <h1 id='linkedInh1'>LinkedIn</h1>
-      <a href='https://www.linkedin.com/in/brady-buchholz-032b3a221/' target='_blank' rel='noreferrer'><img src={LinkedIn} id='linkedIn'  alt='LinkedIn Profile'></img></a>
+      <h1>Phone Number:</h1>
+      <p id="phoneNumber">(614) 586-3474</p>
+      <h1>Email Address:</h1>
+      <a href="mailto:bradybuchholz.dev@gmail.com" id="emailAddress">
+        BradyBuchholz.dev@gmail.com
+      </a>
+      <br></br>
+      <br></br>
+      <h1>GitHub</h1>
+      <a href="https://github.com/Brady1052" target="_blank" rel="noreferrer">
+        <img src={GitHub} id="gitHub" alt="GitHub Profile"></img>
+      </a>
+      <div>
+        <h1 id="linkedInh1">LinkedIn</h1>
+        <a
+          href="https://www.linkedin.com/in/brady-buchholz-032b3a221/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={LinkedIn} id="linkedIn" alt="LinkedIn Profile"></img>
+        </a>
       </div>
     </div>
-
   );
 }
