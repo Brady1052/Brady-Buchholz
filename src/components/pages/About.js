801d14de87b60b43542b426b1a3eaaee8e75f828
Brady Buchholz <bradybuchholz@gmail.com>
2022-04-16 20:55:29 -0400

    rearranged order of projects, added comments to projects, and rewrote a few lines in the project section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,15 +13,15 @@ export default function About() {
           <p id="pAboutMe">
             I am a recent college graduate seeking a full time front end web
             development position. Recently I earned a certificate in full stack
-            web development from the Ohio State University College of
+            web development from The Ohio State University College of
             Engineering. The technologies taught in the bootcamp were HTML5,
             CSS3, JavaScript, Node js, Express, React js, Bootstrap, JQuery,
             MySQL, GraphQL, and MongoDB. I continue to grow my skills as a
             developer every day as development is more than just a career path,
-            but a hobby and natural interest of mine. I enjoy working on and
-            learning web development on my own and continue to take courses, and
-            work on projects every week. I encourage anybody interested in
-            finding out more about my work to check out the portfolio section.
+            but a hobby and passion of mine. I enjoy working on and learning web
+            development on my own and continue to take courses, and work on
+            projects every week. I encourage anybody interested in finding out
+            more about my work to check out my portfolio section.
           </p>
           <img
             id="certificateImg"
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -7,36 +7,16 @@ import fitnessTracker from '../../images/Workout_Tracker.png';
 import employeeManagementSystem from '../../images/Employee_Management_System.png';
 export default function Portfolio() {
   return (
-    <div>
-      <br></br>
-      <h1 id="firstWorkText">The Movie DB</h1>
-      <p>
-        Below is "The Movie DB". An application created by my group and I that
-        allows users to see and leave reviews for movies. The application allows
-        users to create an account, and/or login if they want to leave reviews.
-        Below is a link to the website, and our repository.
-      </p>
-      <strong>Technologies used:</strong> Handlebars.js, Node and Express Web
-      Server, SQL, Get and Post Routes, Heroku, Sessions, Bcrypt (to hash the
-      user's password in the database)
-      <div class="firstProject">
-        <a id="repoLink" href="https://github.com/Brady1052/Group-Project-2">
-          Click Here to see Repository
-        </a>
-        <a href="https://arcane-basin-64105.herokuapp.com/">
-          {' '}
-          <img id="movie_db" src={movieDBImage} alt="The Movie DB Screenshot" />
-        </a>
-      </div>
-      <div>
+    <div id="projectContainer">
+      {/* Weather Dashboard container */}
+      <div id="weatherDashboardContainer">
         <br></br>
         <h3>Weather Dashboard</h3>
         <p>
           The Weather Dashboard project was created mainly as a way to gain and
           show experience using Javascript. The app uses fetch to make XML
-          request to the Open Weather API, and then uses Javascript to
-          manipulate the DOM and present the data received. The entire app was
-          written by me, therefore I also wrote the HTML, and CSS.
+          requests to the Open Weather API, and then uses Javascript to
+          manipulate the DOM and present the data received.
         </p>
         <strong>What the app does:</strong>
         <p>
@@ -54,50 +34,48 @@ export default function Portfolio() {
           HTML5, CSS, Bootstrap, Javascript(ES6), Fetch Requests, Open Weather
           API, Promise Handling, DOM Manipulation, Local Storage
         </p>
+        <a id="repoLink" href="https://github.com/Brady1052/Weather-Dashboard">
+          Click Here to see Repository
+        </a>
+
+        <a href="https://brady1052.github.io/Weather-Dashboard/">
+          <img
+            id="weather-dashboard"
+            src={weatherDashboard}
+            alt="Weather Dashboard Screenshot"
+          />
+        </a>
+      </div>
+      <br></br>
+      {/* MovieDB Container */}
+      <div id="movieDbContainer">
+        <h1 id="firstWorkText">The Movie DB</h1>
+        <p>
+          Below is "The Movie DB". An application created by my group and I that
+          allows users to see and leave reviews for movies. The application
+          allows users to create an account, and/or login if they want to leave
+          reviews. Below is a link to the website, and our repository.
+        </p>
+        <strong>Technologies used:</strong> Handlebars.js, Node and Express Web
+        Server, SQL, Get and Post Routes, Heroku, Sessions, Bcrypt (to hash the
+        user's password in the database)
         <div class="firstProject">
-          <a
-            id="repoLink"
-            href="https://github.com/Brady1052/Weather-Dashboard"
-          >
+          <a id="repoLink" href="https://github.com/Brady1052/Group-Project-2">
             Click Here to see Repository
           </a>
-
-          <a href="https://brady1052.github.io/Weather-Dashboard/">
+          <a href="https://arcane-basin-64105.herokuapp.com/">
+            {' '}
             <img
-              id="weather-dashboard"
-              src={weatherDashboard}
-              alt="Weather Dashboard Screenshot"
+              id="movie_db"
+              src={movieDBImage}
+              alt="The Movie DB Screenshot"
             />
           </a>
         </div>
       </div>
-      <div>
-        <br></br>
-        <h3>Employee Management System</h3>
-        <p>
-          The Employee Management System allows users to create, add, and update
-          employees, roles, and departments within a company. Click the image
-          for a brief demonstration of the app.
-        </p>
-        <strong> Technologies used: </strong> npm Inquirer, mySQL2, and
-        Javascript.
-        <div class="firstProject" />
-        <a
-          id="repoLink"
-          href="https://github.com/Brady1052/Employee-Management-System"
-        >
-          Click Here to see Repository
-        </a>
-        <a href="https://watch.screencastify.com/v/lqIrv81yhnMIpUErRmmc">
-          <img
-            id="em_System"
-            src={employeeManagementSystem}
-            alt="Employee Management System"
-          />
-        </a>
-      </div>
-      <div>
-        <br></br>
+      <br></br>
+      {/* Work Day Scheduler Container */}
+      <div id="Work Day Scheduler Container">
         <h3>Work Day Scheduler</h3>
         <p>
           {' '}
@@ -121,39 +99,65 @@ export default function Portfolio() {
           </a>
         </div>
       </div>
-      <div>
-        <br></br>
-        <h3>Note Taker</h3>
+      <br></br>
+      {/* Employee Managment System Container */}
+      <div id="emsContainer">
+        <h3>Employee Management System</h3>
         <p>
-          This application allows the user to add and save notes to the note
-          taker.
+          The Employee Management System allows users to create, add, and update
+          employees, roles, and departments within a company. Click the image
+          for a brief demonstration of the app.
         </p>
-        <strong>Technologies used: </strong>Express, Heroku, JSON.
+        <strong> Technologies used: </strong> npm Inquirer, mySQL2, and
+        Javascript.
         <div class="firstProject" />
-        <a id="repoLink" href="https://github.com/Brady1052/Note-Taker">
+        <a
+          id="repoLink"
+          href="https://github.com/Brady1052/Employee-Management-System"
+        >
           Click Here to see Repository
         </a>
-        <a href="https://note-take-brady.herokuapp.com/">
-          <img id="note_taker" src={noteTaker} alt="Note Taker App" />
+        <a href="https://watch.screencastify.com/v/lqIrv81yhnMIpUErRmmc">
+          <img
+            id="em_System"
+            src={employeeManagementSystem}
+            alt="Employee Management System"
+          />
         </a>
-      </div>
-      <div>
         <br></br>
-        <h3>Fitness Tracker</h3>
-        <p>
-          "For this assignment, you'll create a workout tracker. You have
-          already been provided with the front end code in the Develop folder.
-          This assignment will require you to create Mongo database with a
-          Mongoose schema and handle routes with Express." - Courtesy of The
-          Ohio State Full Stack Bootcamp <br></br>
-        </p>
-        <div class="firstProject" />
-        <a id="repoLink" href="https://github.com/Brady1052/Fitness-Tracker">
-          Click Here to see Repository
-        </a>
-        <a href=" https://sheltered-tor-12441.herokuapp.com/">
-          <img id="note_taker" src={fitnessTracker} alt="Note Taker App" />
-        </a>
+        <div id="noteTakerContainer">
+          <h3>Note Taker</h3>
+          <p>
+            This application allows the user to add and save notes to the note
+            taker.
+          </p>
+          <strong>Technologies used: </strong>Express, Heroku, JSON.
+          <div class="firstProject" />
+          <a id="repoLink" href="https://github.com/Brady1052/Note-Taker">
+            Click Here to see Repository
+          </a>
+          <a href="https://note-take-brady.herokuapp.com/">
+            <img id="note_taker" src={noteTaker} alt="Note Taker App" />
+          </a>
+        </div>
+        <br></br>
+        <div id="fitnessTrackerContainer">
+          <h3>Fitness Tracker</h3>
+          <p>
+            "For this assignment, you'll create a workout tracker. You have
+            already been provided with the front end code in the Develop folder.
+            This assignment will require you to create Mongo database with a
+            Mongoose schema and handle routes with Express." - Courtesy of The
+            Ohio State Full Stack Bootcamp <br></br>
+          </p>
+          <div class="firstProject" />
+          <a id="repoLink" href="https://github.com/Brady1052/Fitness-Tracker">
+            Click Here to see Repository
+          </a>
+          <a href=" https://sheltered-tor-12441.herokuapp.com/">
+            <img id="note_taker" src={fitnessTracker} alt="Note Taker App" />
+          </a>
+        </div>
       </div>
     </div>
   );
